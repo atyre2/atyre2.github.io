70294399083a8b08161ab0c474ebde38a3705143
Drew Tyre <atyre2@unl.edu>
2017-01-16 08:08:50 -0600

    breaking model selection post into 2 pieces, resetting build.R

--- build.R
+++ build.R
@@ -1,19 +1,19 @@
 build <- function(input){
   
-  #knitr::render_jekyll()
+  knitr::render_jekyll()
 
   # input/output filenames are passed as two additional arguments to Rscript
   d = gsub('^_[a-zA-Z]+\\/|[.][a-zA-Z]+$', '', input)
-  # knitr::opts_chunk$set(
-  #   fig.path   = sprintf('figure/%s/', d),
-  #   cache.path = sprintf('_cache/%s/', d)
-  # )
+  knitr::opts_chunk$set(
+     fig.path   = sprintf('figure/%s/', d),
+     cache.path = sprintf('_cache/%s/', d)
+  )
   
-  output = sprintf('%s-%s.md',Sys.Date(),d)
+  output = sprintf('_posts/%s-%s.md',Sys.Date(),d)
 
-  #knitr::opts_knit$set(width = 70,
-  #                     base.url = "/")
-  rmarkdown::render(input, output_file=output, 
+  knitr::opts_knit$set(width = 70,
+                       base.url = "/")
+  knitr::knit(input, output=output, 
                     quiet = TRUE, 
                     encoding = 'UTF-8', 
                     envir = new.env())
