44b2aa3ab2b33fc222424d5cd6ff17f8879e7ba2
Drew Tyre <atyre2@unl.edu>
2017-02-22 16:04:59 -0600

    fix build.R

--- build.R
+++ build.R
@@ -2,10 +2,10 @@ build <- function(input){
   
   #knitr::render_markdown()
   # extract YAML header
-  input_file <- readLines(input, warn=FALSE)
-  header <- grep("---",input_file)
-  if (length(header) > 2) stop("too much YAML!")
-  yaml_header <- input_file[header[1]:header[2]]
+  # input_file <- readLines(input, warn=FALSE)
+  # header <- grep("---",input_file)
+  # if (length(header) > 2) stop("too much YAML!")
+  # yaml_header <- input_file[header[1]:header[2]]
 
   # input/output filenames are passed as two additional arguments to Rscript
   d = gsub('^_[a-zA-Z]+\\/|[.][a-zA-Z]+$', '', input)
@@ -18,11 +18,11 @@ build <- function(input){
 
   knitr::opts_knit$set(width = 70,
                        base.url = "/")
-  rmarkdown::render(input, output_format = "md_document")
-  md_doc <- readLines(paste0("_drafts/",d,".md"))
-  writeLines(c(yaml_header, md_doc),output)
-  # knitr::knit(input, output=output, 
-  #                   quiet = TRUE, 
-  #                   encoding = 'UTF-8', 
-  #                   envir = new.env())
+  # rmarkdown::render(input, output_format = "md_document")
+  # md_doc <- readLines(paste0("_drafts/",d,".md"))
+  # writeLines(c(yaml_header, md_doc),output)
+  knitr::knit(input, output=output, 
+                     quiet = TRUE, 
+                     encoding = 'UTF-8', 
+                     envir = new.env())
 }
